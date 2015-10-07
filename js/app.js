4bb626431b98922d7989bb6d5ece7ae8f34ea210
Cee <cee@chu2byo.com>
2015-10-08 00:29:38 +0800

    Responsive now

--- js/app.js
+++ js/app.js
@@ -6,4 +6,10 @@ window.onload = function() {
 function initPerfectScrollBar() {
   var cart = document.querySelector('.cart');
   Ps.initialize(cart);
+
+	var $toggle = document.querySelector(".site__right-sidebar-toggle");
+	$toggle.addEventListener("click", function() {
+	  document.body.classList.toggle("js-show-right-sidebar");
+	});
 }
+
